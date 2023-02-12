063fc2de7d825987dc3e72fd380ee7a0086f30fa
hiruni25 <hirudesilva25@gmail.com>
2023-02-13 02:03:55 +0530

    changes in headers

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -44,12 +44,11 @@ const Header = () => {
               >
                 <figure className="avatar avatar-nav">
                   <img
-                    src={user.avatar && user.avatar.url}
-                    alt={user && user.name}
+                    src={"https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="}
                     className="rounded-circle"
                   />
                 </figure>
-                <span>{user && user.name}</span>
+                <span>{user && user.email}</span>
               </Link>
 
               <div
